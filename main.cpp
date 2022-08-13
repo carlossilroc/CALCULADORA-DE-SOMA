d9feae54bf54e12ec1709d63ae88435b3a096a1b
carlossilroc <105683772+carlossilroc@users.noreply.github.com>
2022-08-12 21:29:52 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+//Carlos Silva Rocha, carlossilroc@gmail.com
+using namespace std;
+int main(int argc, char** argv) {
+			
+				int a, b, c, d, e, f, g;
+				system("color f5");
+				cout << "" << endl << endl << endl;
+				cout << "		********************************************************" << endl;
+				cout << "		*****                  ***********                 *****" << endl;
+				cout << "		***              V11082022 ENSINO MEDIO              ***" << endl;
+				cout << "		**     Prof. Carlos Rocha, carlossilroc@gmail.com     **" << endl;
+				cout << "		**                 CALCULADORA DE SOMA                **" << endl;
+				cout << "		********************************************************" << endl;
+				cout << "		**                 Digite os valores                  **" << endl;
+				cout << "		**                                                    **" << endl;
+				cout << "		                      Valor I: "; cin >> a;
+				cout << "		                      Valor II: "; cin >> b;
+				cout << "		                      Valor III: "; cin >> c;
+				cout << "		                      Valor IV: "; cin >> d;
+				cout << "		                      Valor V: "; cin >> f;
+				cout << "		**                                                    **" << endl;
+				cout << "		********************************************************" << endl;
+				cout << "		********************************************************" << endl << endl;
+				g = a+b+c+d+e+f;
+				cout << "		                     VALOR TOTAL: "; cout << g; cout << " " << endl << endl;
+				system("pause");
+				system ("cls");
+						
+	system ("pause");
+	return 0;
+}
+
